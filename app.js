565137d4da1a84fc6eb126834f184209966baaba
Hiren Thacker <hithacker@gmail.com>
2017-07-17 15:58:12 +0530

    Add docker-compose file.
    Added docker-compose file. It mounts the volume so the changes can be reflected instantly.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Yo World');
+    res.send('Hi World');
 });
 
 app.listen(3000, function () {
