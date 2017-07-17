ef71a7bf74b5926e50af3a35e34ce2ef23b093ef
Hiren Thacker <hithacker@gmail.com>
2017-07-17 16:43:26 +0530

    add mongo
    add mongo

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://mongo:27017");
 
 app.get('/', function (req, res) {
     res.send('Hi World');
